1cc13108504f3768d82bbd5ab2f2b80b3136114f
TroyAWilson <wilsontr@sonoma.edu>
2026-06-01 14:56:06 -0700

    backrooms

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const catImages = [
-    "catindryer.jpg", "20250406_180529.jpg", "catinfridge.jpg",  
-    "PXL_20251103_000841904.jpg", "PXL_20251103_000842723.jpg", "PXL_20251103_022555781.jpg", 
+    "catindryer.jpg", "PXL_20260531_143439042.jpg", "PXL_20260531_151157574.jpg", "20250406_180529.jpg", "catinfridge.jpg",  
+    "PXL_20251103_000841904.jpg", "PXL_20251103_000842723.jpg", "PXL_20260528_013031843.jpg", "PXL_20251103_022555781.jpg", 
     "PXL_20260327_231953866.jpg", "Snapchat-668973871.jpg", "Snapchat-2123923997.jpg", 
     "Snapchat-4195133542.jpg", "Snapchat-6413555412.jpg",
 ];
